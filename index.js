a5cb10a070af9e8bb1ead8b5bd788257323e0470
Apolojize <84992745+Apolojize@users.noreply.github.com>
2021-08-05 20:30:16 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js-self");
+var setTitle = require('console-title');
+const {token} = require("./config.json");
+var color = require("chalk");
+require('colors');
+const client = new Discord.Client();
+var readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+console.log(`
+█████╗ ██╗   ██╗████████╗ ██████╗       ██████╗ ██╗   ██╗███╗   ███╗██████╗ 
+██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗      ██╔══██╗██║   ██║████╗ ████║██╔══██╗
+███████║██║   ██║   ██║   ██║   ██║█████╗██████╔╝██║   ██║██╔████╔██║██████╔╝
+██╔══██║██║   ██║   ██║   ██║   ██║╚════╝██╔══██╗██║   ██║██║╚██╔╝██║██╔═══╝ 
+██║  ██║╚██████╔╝   ██║   ╚██████╔╝      ██████╔╝╚██████╔╝██║ ╚═╝ ██║██║
+                               BY APOLOJIZE                                                                                                                               
+`)
+setTitle("Auto-bump by Apolojize");
+if(!token) {console.log('Token invalid (config.json)'.red)}
+console.log("[!] Connecting ...".cyan)
+client.on('ready', async() => {
+  console.log(`[!] Logged in ${client.user.tag} (${client.user.id})`.cyan)
+  let channelids =  await question('[?] What\'s is the channel id ? '.cyan);
+  const channel = client.channels.cache.get(channelids);
+  if(!channel) {console.log('Channel invalid.'.red)}
+  channel.send("!d bump")
+  setInterval(async () => {
+    channel.send("!d bump")
+  }, 726000)
+})
+    client.login(token);
+
+    function question(string) {
+      return new Promise((res) => {
+          rl.question(string, (answer) => res(answer))
+      })
+  }
---
+++ install.bat
@@ -0,0 +1,5 @@
+npm i chalk
+npm i colors
+npm i discord.js-self
+npm i console-title
+npm i readline
